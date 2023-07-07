9e7f566b7421c38878542196bdca929eacaad157
Avadhut Savale <96470055+Reverseco@users.noreply.github.com>
2023-07-07 14:28:47 +0530

    Add files via upload

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql2");
+var db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "savaLe@2021",
+  database: "dbms_project",
+});
+
+    db.connect(function (err) {
+      if (err) {
+        console.log("Error in db connectivity");
+      } else {
+        console.log("connected to database");
+      }
+    });
+
+module.exports=db;
+
